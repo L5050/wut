ebdbfcccf88c332f1e883a6bb686f4903b8938d5
Lily Lawliet <dr.trayaurus61@gmail.com>
2025-11-29 15:10:37 -0800

    fix errors with RPL that prevent it from being loaded into color splash

--- libraries/wutcrt/crt0_rpl.s
+++ libraries/wutcrt/crt0_rpl.s
@@ -16,10 +16,7 @@ __rpl_start:
 
 load:
    # Load
-   bl __init_wut
    # rpl files use wutmalloc instead of the custom heap
-   bl __init_wut_malloc
-   bl __eabi
    lwz 3, 0x8(1)
    lwz 4, 0xC(1)
    bl rpl_entry
